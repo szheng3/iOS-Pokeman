b12ccc8dd8160a2951326253b9c1080b0afdc37d
Shuai <szheng3@uci.edu>
2020-04-14 15:59:50 -0500

    Initial Commit

---
+++ p2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  p2
+//
+//  Created by Shuai Zheng on 4/14/20.
+//  Copyright © 2020 Shuai Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
