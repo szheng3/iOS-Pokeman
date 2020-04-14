f5bdb58e72f08596881ebac8ede6b64b9ab1bc88
Shuai <szheng3@uci.edu>
2020-04-14 18:27:05 -0500

    csv

--- p2/PokeCell.swift
+++ p2/PokeCell.swift
@@ -5,6 +5,34 @@
 
 import UIKit
 
-class PokeCell: UICollectionViewCell{
+class PokeCell: UICollectionViewCell {
 
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var avatar: UIImageView!
+
+    var pokemon: Pokes!;
+//
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+
+        layer.cornerRadius = 5.0
+    }
+
+
+    func configureCell(_ pokemon: Pokes) {
+
+        self.pokemon = pokemon
+
+        name.text = self.pokemon.name.capitalized
+        avatar.image = UIImage(named: "\(self.pokemon.pokedexId)")
+
+    }
+
+//    func configureCell(_ pokemon: Int) {
+//
+//
+//        name.text = "\(pokemon)"
+//        avatar.image = UIImage(named: "\(pokemon)")
+//
+//    }
 }
--- p2/ViewController.swift
+++ p2/ViewController.swift
@@ -10,33 +10,61 @@ import UIKit
 
 class ViewController: UIViewController,UICollectionViewDelegate, UICollectionViewDataSource {
     @IBOutlet weak var pokemanView: UICollectionView!
+    var pokemon = [Pokes]()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         pokemanView.dataSource = self
         pokemanView.delegate = self
+        parsePokemonCSV()
+
         // Do any additional setup after loading the view.
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 
-        return 6
+        return pokemon.count
+    }
+
+    func parsePokemonCSV() {
+
+        let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")!
+
+        do {
+
+            let csv = try CSV(contentsOfURL: path)
+            let rows = csv.rows
+
+            for row in rows {
+
+                let pokeId = Int(row["id"]!)!
+                let name = row["identifier"]!
+
+                let poke = Pokes(name: name, pokedexId: pokeId)
+                pokemon.append(poke)
+
+            }
+
+        } catch let err as NSError {
+
+            print(err.debugDescription)
+        }
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
 
+
+            cell.configureCell(pokemon[indexPath.row]);
             return cell
         }
         return UICollectionViewCell();
     }
 
     public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("ggg")
-        let a = "gg"
-        performSegue(withIdentifier: "DetailsViewController", sender: a)
+        performSegue(withIdentifier: "DetailsViewController", sender: self)
 
 
     }
